1bbae089fc48941bf6a86576315187e7db6f0622
Jonathan Rezende <jonathan@jode.com.br>
2018-05-19 23:08:43 -0300

    action stuff

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 $router->get("/timeline", ["uses" => "TimelineController@allUsers"]);
 $router->get("/timeline/{followId}", ["uses" => "TimelineController@specificUser"]);
+
+$router->get("/application", ["uses" => "ApplicationController@list"]);
 $router->post("/application", ["uses" => "ApplicationController@add"]);
 $router->put("/application/{id}", ["uses" => "ApplicationController@update"]);
-$router->put("/application/generateKey/{id}", ["uses" => "ApplicationController@generateKey"]);
+$router->get("/application/generateKey/{id}", ["uses" => "ApplicationController@generateKey"]);
+
+$router->get("/action", ["uses" => "ActionController@list"]);
+$router->post("/action", ["uses" => "ActionController@add"]);
+$router->put("/action/{id}", ["uses" => "ActionController@update"]);
