b631e2fa9fefafb5900234d2725feda606055e17
Jonathan Rezende <jonathan@jode.com.br>
2018-05-20 02:18:42 -0300

    link

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ $router->post("/application", ["uses" => "ApplicationController@add", "middlewar
 $router->put("/application/{id}", ["uses" => "ApplicationController@update", "middleware" => "auth"]);
 $router->get("/application/generateKey/{id}", ["uses" => "ApplicationController@generateKey", "middleware" => "auth"]);
 
-$router->get("/action", ["uses" => "ActionController@list", "middleware" => "auth"]);
-$router->post("/action", ["uses" => "ActionController@add", "middleware" => "auth"]);
-$router->put("/action/{id}", ["uses" => "ActionController@update", "middleware" => "auth"]);
+$router->get("/action", ["uses" => "ActionController@list"]);
+$router->post("/action", ["uses" => "ActionController@add"]);
+$router->put("/action/{id}", ["uses" => "ActionController@update"]);
+
+$router->get("/history", ["uses" => "HistoryController@list"]);
+$router->post("/history", ["uses" => "HistoryController@add"]);
